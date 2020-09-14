8efe6da1a593b6fa332f13fe2466c06b71028f98
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-09-15 06:54:35 +0900

    feat: GraphQL dev

--- apps/api/src/apollo-server.ts
+++ apps/api/src/apollo-server.ts
@@ -1,14 +1,53 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+    category: PhotoCategory
+  }
   type Query {
-    hello: String
+    totalPhotos: Int!
+    allPhotos: [Photo]!
+  }
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory = PORTRAIT
+    description: String
+  }
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
 
+let id = 0;
+const photos = [];
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    totalPhotos: () => photos.length,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const newPhoto = {
+        id: id++,
+        ...args.input,
+      };
+      photos.push(args);
+      return newPhoto;
+    },
+  },
+  Photo: {
+    // トリビアリゾルバ
+    url: (parent) => `http://example.com/img/${parent.id}`,
   },
 };
 
